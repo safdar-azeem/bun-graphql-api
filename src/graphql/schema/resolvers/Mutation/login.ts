f469482e804c74ecdcd33c8df7048848cd95b556
safdar-azeem <safdarazeem00@gmail.com>
2023-09-19 13:37:38 +0500

    Update login.ts

--- src/graphql/schema/resolvers/Mutation/login.ts
+++ src/graphql/schema/resolvers/Mutation/login.ts
@@ -5,9 +5,11 @@ import type { MutationResolvers } from './../../../types.generated'
 export const login: NonNullable<MutationResolvers['login']> = async (_parent, { email, password }, _ctx) => {
    try {
       const user = await UserModel.findOne({ email })
+
       if (!user) {
          throw new GraphQLError('User not found')
       }
+
       const isMatch = await user.comparePassword(password)
       if (!isMatch) {
          throw new GraphQLError('Password is incorrect')
